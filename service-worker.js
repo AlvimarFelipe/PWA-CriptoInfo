e707aeb9ee4b41532c8ce09babaf82e11b144a46
Gabriel Feijó <92588394+GabrielFeijo@users.noreply.github.com>
2022-10-12 04:07:59 +0000

    Service Worker

--- service-worker.js
+++ service-worker.js
@@ -9,54 +9,8 @@ self.addEventListener('install', event => {
       .then(cache => cache.addAll([
 
         './home.html',
-        './Como-Investir.html',
-        './Ajuda.html',
-        './O-que-é.html',
-        './index.html',
-        './compra.html',
-        './venda.html',
-        './Minhas_criptomoedas.html',
-        
-
-        './assets/css/fontawesome-all.min.css',
-        './assets/css/main.css',
-        './assets/css/bloqueio.css',
-
-        './assets/js/breakpoints.min.js',
-        './assets/js/browser.min.js',
-        './assets/js/filtro.js',
-        './assets/js/jquery.min.js',
-        './assets/js/main.js',
-        './assets/js/util.js',
-        './assets/js/bloqueio.js',
-        './assets/js/compras.js',
-
-        
-
-        'images/carteira de cripto.jpg',
-        'images/cripto no Brasil.jpg',
-        'images/cripto1.png',
-        'images/cripto2.png',
-        'images/cripto3.png',
-        'images/criptomoedas valorização.jpg',
-        'images/funeral_RE.jpg',
-        'images/Home.png',
-        'images/investimento.jpg',
-        'images/logo.png',
-        'images/mercado de cripto.jpg',
-
-
-        
-        'images/imgs/128.png',
-        'images/imgs/144.png',
-        'images/imgs/152.png',
-        'images/imgs/167.png',
-        'images/imgs/180.png',
-        'images/imgs/196.png',
-        'images/imgs/256.png',
-        'images/imgs/512.png',
-        'images/imgs/1024.png',
-                
+        './index.html'
+  
       ]))
   );
 });
